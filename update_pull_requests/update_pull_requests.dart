d02f9a5c52b02f5c4be77debc30b402118d0941b
Nate Wilson <nate.w5687@gmail.com>
2024-08-12 17:09:30 -0600

    cleanup

--- update_pull_requests/update_pull_requests.dart
+++ update_pull_requests/update_pull_requests.dart
@@ -9,7 +9,18 @@ import 'package:graphql/client.dart';
 
 import 'token.dart';
 
-Future<QueryResult<StringBuffer>> query(String filepath) {
+void main() async {
+  final String prList = await query('update_pull_requests/pr_query.gql');
+  final String text = """\
+part of 'pr_data.dart';
+
+const flutterPRs = <PullRequest>[$prList];
+""";
+
+  File('lib/stats/pr_data.g.dart').writeAsString(text);
+}
+
+Future<String> query(String filepath) async {
   final client = GraphQLClient(
     cache: GraphQLCache(),
     link: HttpLink(
@@ -17,26 +28,26 @@ Future<QueryResult<StringBuffer>> query(String filepath) {
       defaultHeaders: {'Authorization': 'Bearer $token'},
     ),
   );
-  return client.query(
-    QueryOptions<StringBuffer>(
-      document: gql(File(filepath).readAsStringSync()),
-      parserFn: parseQuery,
-    ),
+  final queryOptions = QueryOptions<String>(
+    document: gql(File(filepath).readAsStringSync()),
+    parserFn: parse,
   );
+
+  return (await client.query(queryOptions)).parsedData!;
 }
 
-StringBuffer parseQuery(Map<String, dynamic> data) {
-  final {'user': {'pullRequests': {'nodes': nodes as List<dynamic>}}} = data;
-  final prs = StringBuffer()..writeln();
+String parse(Map<String, dynamic> data) {
+  final buffer = StringBuffer()..writeln();
 
+  final {'user': {'pullRequests': {'nodes': nodes as List}}} = data;
   for (final pr in nodes) {
     final {
       'title': title as String,
       'url': url as String,
       'additions': additions as int,
       'deletions': deletions as int,
-      'labels': {'nodes': labels as List<dynamic>},
-    } = pr! as Map;
+      'labels': {'nodes': labels as List},
+    } = pr;
 
     final bool refactor = labels.any((label) => label['name'] == 'refactor');
 
@@ -49,20 +60,8 @@ StringBuffer parseQuery(Map<String, dynamic> data) {
   ),
 """;
 
-    prs.writeln(parsed.trimRight());
+    buffer.writeln(parsed.trimRight());
   }
 
-  return prs;
-}
-
-void main() async {
-  final prs = (await query('update_pull_requests/pr_query.gql')).parsedData;
-
-  File('lib/stats/pr_data.g.dart').writeAsString(
-    """\
-part of 'pr_data.dart';
-
-const flutterPRs = <PullRequest>[$prs];
-""",
-  );
+  return buffer.toString();
 }
